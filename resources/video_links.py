8982e286c9db77c27497e963a06c52e4dd895b2c
antONget <a.l.ponomare@mail.ru>
2024-01-25 20:17:40 +0300

    кнопки

--- resources/video_links.py
+++ resources/video_links.py
@@ -1,6 +1,6 @@
 LINK_VIDEO = {
-    'video0': 'https://youtu.be/-SrQWaf1Uhg?si=557hYRl4EVVFs6Hb',
-    'video1': 'https://youtu.be/S2en8ZAnRvE?si=RxuLsWQfloL_7AxW',
-    'video2': 'https://youtu.be/4IRfDjLoGJU?si=szokpjrsTRiUPb2S',
-    'video3': 'https://youtu.be/jbYnxZkjcxw?si=lGzxzcNeE3mzL-Mu',
+    'video0': 'https://youtu.be/DbxyuIV_Gyw?si=CJWJb7BhRzYkC1zH',
+    'video1': 'https://youtu.be/VHDeS1tT6IU?si=50Gl30hrVswlI9Nc',
+    'video2': 'https://youtu.be/u29-r8XmiQc?si=fAd1v84PUhk2LvNA',
+    'video3': 'https://youtu.be/2XB_KWUXwK4?si=43rDPwesM6owfQi5',
 }
